7c24a2cf3677f6fc46d944e6def36580769febe2
phuong3030 <sir1001dem@gmail.com>
2014-06-03 00:54:39 +0700

    Using sqlite

--- Gemfile
+++ Gemfile
@@ -4,10 +4,10 @@ source 'https://rubygems.org'
 gem 'rails'
 
 # Use sqlite3 as the database for Active Record
-# gem 'sqlite3'
+gem 'sqlite3'
 
 # Use postgreSQL as production database
-gem 'pg'
+#gem 'pg'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.2'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 SecFly::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+  # Code i not reloaded between requests.
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
