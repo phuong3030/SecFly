0414882fb02b547d7a21c438a2605b60fdc4a99e
phuong3030 <sir1001dem@gmail.com>
2014-06-03 16:29:50 +0700

    Choosing place dialog

--- app/assets/javascripts/home/travel.js
+++ app/assets/javascripts/home/travel.js
@@ -1,17 +1,44 @@
 (function ($) {
+	
+	var placeTitle = $(".place-title>h2"),
+		 fromInputField = $("#from"),
+		 toInputField = $("#to");
 
 	$("#dtBox").DateTimePicker({});
 	$("#from").magnificPopup({
 		items: {
 			src: "#place-popup",
 			type: "inline"
-		}	
+		},
+		callbacks: {
+			beforeOpen: function() {
+				placeTitle.html("From");
+			}
+		}
 	});
 	$("#to").magnificPopup({
 		items: {
 			src: "#place-popup",
 			type: "inline"
+		},
+		callbacks: {
+			beforeOpen: function() {
+				placeTitle.html("To");
+			}
 		}
+	});
+	$("#place-popup").delegate(".airport-name", "click", function(e) {
+		$.magnificPopup.close();
+			
+		if (placeTitle.html() === "From") {
+
+			fromInputField.val(e.currentTarget.innerHTML);	
+		} else if (placeTitle.html() === "To") { 
+
+			toInputField.val(e.currentTarget.innerHTML);	
+		}
+
+		return false;
 	});
 
 }) (jQuery);
