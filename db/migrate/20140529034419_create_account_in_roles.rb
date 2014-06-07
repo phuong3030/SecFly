43c5bbf783fb7aa5ffedcd431ece470fe4297e44
phuong3030 <sir1001dem@gmail.com>
2014-06-07 09:14:22 +0700

    fix migration

--- db/migrate/20140529034419_create_account_in_roles.rb
+++ db/migrate/20140529034419_create_account_in_roles.rb
@@ -8,12 +8,9 @@ class CreateAccountInRoles < ActiveRecord::Migration
 
 		add_index :account_in_roles, :account_id
 		add_index :account_in_roles, :role_id
-
-		remove_column :accounts, :roles
 	end
 
 	def self.down
-		add_column :accounts, :roles
 		drop_table :user_in_roles
 	end
 end
