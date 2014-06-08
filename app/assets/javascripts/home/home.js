52eda03f0a9d0521606460b54d15d2ebdaf0fa9c
phuong3030 <sir1001dem@gmail.com>
2014-06-08 18:51:04 +0700

    Fix banner height style in phone

--- app/assets/javascripts/home/home.js
+++ app/assets/javascripts/home/home.js
@@ -1,10 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $('.dropdown').on('show.bs.dropdown', function (e) {
-	            $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-	                    });
+	$(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
 
         // ADD SLIDEUP ANIMATION TO DROPDOWN //
- $('.dropdown').on('hide.bs.dropdown', function (e) {
-                    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-                             });
+$('.dropdown').on('hide.bs.dropdown', function (e) {
+	$(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+});
