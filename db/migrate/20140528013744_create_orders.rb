270a5c7cff9ffc7b4c45a7e6ad3ec99a2dcfe676
phuong3030 <sir1001dem@gmail.com>
2014-06-09 08:55:00 +0700

    Add new column in order table

--- db/migrate/20140528013744_create_orders.rb
+++ db/migrate/20140528013744_create_orders.rb
@@ -3,13 +3,15 @@ class CreateOrders < ActiveRecord::Migration
 		create_table :orders do |t|
 			t.belongs_to :customer
 
+			t.text :name
 			t.text :from
 			t.text :to
 			t.datetime :depart
 			t.datetime :return
 			t.integer :adult
-			t.integer :seniors
 			t.integer :children
+			t.integer :infant
+			t.integer :payment_method
 			t.integer :status, :default => 0
 			t.timestamps
 		end
