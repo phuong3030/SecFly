530673e6a9ca4e4ed8610f5dedc13d5a49153bd5
phuong3030 <sir1001dem@gmail.com>
2014-06-09 11:30:04 +0700

    fix language dropdown slide effect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,4 +25,13 @@
 		fadeInTime: 100
 	});
 
+	// language dropdown slide effect
+	$('.dropdown').on('show.bs.dropdown', function (e) {
+		$(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+	});
+	
+	$('.dropdown').on('hide.bs.dropdown', function (e) {
+		$(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+	});
+
 })(jQuery);
